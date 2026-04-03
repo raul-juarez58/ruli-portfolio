ef10610bc2b594dad64c13255a9bcdcccc72c29d
Raul Juarez <rauljuarez.fernando@gmail.com>
2026-04-03 17:18:02 -0300

    animaciones scroll en proyectos

--- src/componentes/Proyectos.jsx
+++ src/componentes/Proyectos.jsx
@@ -1,6 +1,20 @@
+import {useEffect, useRef} from "react";
 import "./Proyectos.css";
 
 function Proyectos() {
+   const ref = useRef(); 
+    
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("mostrar");
+                }
+            });
+        });   
+        
+        observer.observe(ref.current);
+    }, []);
 
     const proyectos = [
         {
@@ -18,20 +32,20 @@ function Proyectos() {
     ];
 
   return (
-    <section className="proyectos">
+    <section ref={ref}className="proyectos ocultos">
       <h3>Proyectos</h3>
 
-       {proyectos.map((proyecto, index) => (
-      <div className="card" key={index}>
-        <h4>{proyecto.nombre}</h4>
-        <p>{proyecto.descripcion}</p>
+      {proyectos.map((p, index) => (
+        <div className="card" key={index}>
+            <h4>{p.nombre}</h4>
+            <p>{p.descripcion}</p>
 
-        <div className="links">
-          <a href={proyecto.demo} target="_blank">Ver proyecto</a>
-          <a href={proyecto.codigo} target="_blank">Código</a>
+            <div className="links">
+                <a href={p.demo} target="_blank">Ver proyecto</a>
+                <a href={p.codigo} target="_blank">Codigo</a>
+            </div>
         </div>
-      </div>
-    ))}
+      ))}
     </section>
   );
 }
